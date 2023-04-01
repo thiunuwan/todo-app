4b7f93a355e778467d71ad54714da4efb3da7bd5
thiunuwan <ravinduthiunuwan@gmail.com>
2023-04-01 10:04:27 +0530

    setup structure

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+// export const environment = {
+//   production:false,
+//   firebaseConfig :{
+//     apiKey: "AIzaSyDyXkZke-L-s1SVuI2uweWypKRx8reTIts",
+//     authDomain: "todo-app-9f4cc.firebaseapp.com",
+//     projectId: "todo-app-9f4cc",
+//     storageBucket: "todo-app-9f4cc.appspot.com",
+//     messagingSenderId: "107276157096",
+//     appId: "1:107276157096:web:e9aef02976c0ca446fc29d"
+//   }
+//
+//
+// };
---
+++ src/environments/environment.module.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseConfig :{
+    apiKey: "AIzaSyDyXkZke-L-s1SVuI2uweWypKRx8reTIts",
+    authDomain: "todo-app-9f4cc.firebaseapp.com",
+    projectId: "todo-app-9f4cc",
+    storageBucket: "todo-app-9f4cc.appspot.com",
+    messagingSenderId: "107276157096",
+    appId: "1:107276157096:web:e9aef02976c0ca446fc29d"
+  }
+};
